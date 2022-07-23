8ab0490a947663c8cd8a35976dfe4843fba6549a
Alan ROGER <aa.roger404@gmail.com>
2022-07-23 14:58:56 +0200

    start

--- AAAspeedrun/routes/index.routes.js
+++ AAAspeedrun/routes/index.routes.js
@@ -5,11 +5,12 @@ router.get("/", (req, res, next) => {
   res.render("homepage");
 });
 
-
+/* GET about page */
 router.get("/about",(req,res,next)=>{
   res.render("about");
 })
 
+/* GET login page */
 router.get("/login",(req,res,next)=>{
   res.render("login")
 })
