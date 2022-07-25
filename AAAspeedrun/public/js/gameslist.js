80d6678a1f3ac70c2dcc8a45f618318a47f4d121
Alan ROGER <aa.roger404@gmail.com>
2022-07-25 19:06:09 +0200

    starter

--- AAAspeedrun/public/js/gameslist.js
+++ AAAspeedrun/public/js/gameslist.js
@@ -1,11 +1,3 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("AAAspeedrun JS imported successfully!");
-  },
-  false
-);
-
 import axios from 'axios';
 
 axios({
--- AAAspeedrun/routes/games.routes.js
+++ AAAspeedrun/routes/games.routes.js
@@ -1,7 +1,17 @@
+const { getMaxListeners } = require("../app");
+const SpeedrunClient = require('../node_modules/node-speedrun/lib/Client')
+const speedrun = new SpeedrunClient()
+
+
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-    res.render("games");
+    speedrun.games.get('https://www.speedrun.com/api/v1/games')
+    .then((gamefromAPI)=>{
+        res.render("games",{game:gamefromAPI});
+        console.log(gamefromAPI)
+    })
+    
   });
 
 router.get("/new",(req,res,next)=>{
@@ -15,7 +25,5 @@ router.get("/edit", (req, res, next) => {
 router.get("/:id",(req,res,next)=>{
     res.render("game")
 })
-
-
 
 module.exports = router;
