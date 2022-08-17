bbf24a3e03ab2eab6a2779da46c260b97f3ae2fc
Alan ROGER <aa.roger404@gmail.com>
2022-08-17 15:44:15 +0200

    profile user&runs

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,19 +1,28 @@
 const router = require("express").Router();
+const Runs = require('../models/Run.model.js')
 
 
 
 /* GET profile page */
 router.get("/", (req, res, next) => {
-  console.log("req.session.currentUser:",req.session.currentUser)
-  res.render("profile",{user:req.session.currentUser});
+  console.log("user:", req.session.currentUser)
+  const user = req.session.currentUser
+  Runs
+    .find({ userID: user._id }).sort({date:-1}).limit(20)
+    .populate("userID")
+    .populate("categoryID")
+    .populate("gameID")
+    .then((runsfromDB) => {
+      res.render("profile", { user: req.session.currentUser, runs: runsfromDB })
+    });
 });
 
-router.get("/edit",(req,res,next)=>{
-  res.render("profile-edit",{user:req.session.currentUser})
+router.get("/edit", (req, res, next) => {
+  res.render("profile-edit", { user: req.session.currentUser })
 })
 
 router.get("/favorites", (req, res, next) => {
-  res.render("profile-fav",{user:req.session.currentUser});
+  res.render("profile-fav", { user: req.session.currentUser });
 });
 
 module.exports = router;
