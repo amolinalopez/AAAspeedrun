1c807eb577f5ec467734ce45a5a768c1b6209da1
Alan ROGER <aa.roger404@gmail.com>
2022-08-18 17:59:09 +0200

    game edition

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -7,7 +7,7 @@ const Game = require('../models/Game.model.js')
 const router = require("express").Router();
 
 router.get("/list", (req, res, next) => {
-  Game.find().limit(30)
+  Game.find().sort({title:1})
     .populate("categoriesID")
     .then((gamesFromDB) => { 
       console.log("gamesfromdb:",gamesFromDB)
@@ -21,18 +21,30 @@ router.get("/new", (req, res, next) => {
 
 router.get("/:id/edit", (req, res, next) => {
   Game.findById(req.params.id)
+  .populate("categoriesID")
   .then((gameFromDB)=>{
     res.render("game-edit",{game:gameFromDB});
   })
 });
 
 router.post("/:id/edit",(req,res,next)=>{
-  res.redirect("/:id")
+  const {id} = req.params;
+  const {title,year,cover} = req.body;
+  Game.findByIdAndUpdate(id,{title,year,cover},{new:true})
+  .then((updatedGame)=>{
+    console.log(updatedGame)
+    res.redirect(`/game/${updatedGame._id}`)
+  })
+  .catch((err)=>{
+    console.log('error updating game', err)
+    next(err)
+  })
+  
 })
 
 router.get("/:id", (req, res, next) => {
   Game.findById(req.params.id)
-    .populate("categories")
+    .populate("categoriesID")
     .then((gameFromDB)=>{
       res.render("game",{game:gameFromDB})
     })
